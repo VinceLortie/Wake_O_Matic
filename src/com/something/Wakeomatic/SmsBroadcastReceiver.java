cc8f9b245d4a4545f87840897a08724ef3016821
Vincent Lortie <vincent.lortie@gmail.com>
2014-06-28 18:53:53 -0700

    Initial commit. Mostly an empty Android App

---
+++ src/com/something/Wakeomatic/SmsBroadcastReceiver.java
@@ -0,0 +1,19 @@
+package com.something.Wakeomatic;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Boradcast receiver registered to be called when an SMS comes in.
+ * <p/>
+ * Created by Vincent on 28/06/14.
+ */
+public class SmsBroadcastReceiver extends BroadcastReceiver
+{
+   @Override
+   public void onReceive(Context context, Intent intent)
+   {
+
+   }
+}
